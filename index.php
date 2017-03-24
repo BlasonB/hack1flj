3054ee675bd9942b0f3e86569740989eea809475
BlasonB <benedite.j@gmail.com>
2017-03-24 11:31:26 +0100

    jgjg

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 require('php/header.php');
 ?>
-
-    <div class="container vignets text-center">
+    <div class="container-fluid vignets1 text-center">
+        <div class="infotip">
+            <span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span>
+            <p>Entrez votre recherche en indiquant le nombre de résultats désiré</p>
+        </div>
+    </div>
+    <div class="container vignets2 text-center">
         <div class="transbox">
-            <h3 >Bienvenue sur Eat&Beat</h3>
-            <p>Nous voyons ce que nous mangeons,<br /> que ce soit dans l'assiette, ou sur la sihlouette.</p>
+            <h1 >Bienvenue sur Eat&Beat</h1>
+            <p><span class="glyphicon glyphicon-eye-open" aria-hidden="true"><br />Nous voyons ce que nous mangeons,<br /> que ce soit dans l'assiette, ou sur la sihlouette.<br /><br />
+                <span class="glyphicon glyphicon-search" aria-hidden="true"></span><br />Recherchez ce que vous ingurgitez pour savoir comment l'éliminer !</p>
         </div>
     </div>
 
